a3d2edab1f51fe8f75dc2c10a1c4e49466751be0
ismartrookie <ismartrookie@gmail.com>
2014-04-21 16:47:10 +0800

    Import an Empty Project

---
+++ SMSnatchCar/SMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SMAppDelegate.h
+//  SMSnatchCar
+//
+//  Created by administrator on 14-4-21.
+//  Copyright (c) 2014年 SM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SMSnatchCar/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SMSnatchCar
+//
+//  Created by administrator on 14-4-21.
+//  Copyright (c) 2014年 SM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SMAppDelegate class]));
+    }
+}
